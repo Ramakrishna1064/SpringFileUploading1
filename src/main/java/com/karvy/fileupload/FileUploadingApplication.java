16e6129d466c5d88cd55e46ce78123634f885bb5
krishna.kovilampati <k.ramakrishna1064@gmail.com>
2019-07-02 15:02:06 +0530

    initial commit

---
+++ src/main/java/com/karvy/fileupload/FileUploadingApplication.java
@@ -0,0 +1,19 @@
+package com.karvy.fileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.karvy.fileupload.property.FileStorageProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties({
+    FileStorageProperties.class
+})
+public class FileUploadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadingApplication.class, args);
+	}
+
+}
