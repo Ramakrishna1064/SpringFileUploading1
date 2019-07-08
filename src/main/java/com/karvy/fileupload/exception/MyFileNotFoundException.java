e5a19c25fec42d480acacd80f0c874196284a730
Ramakrishna1064 <40228216+Ramakrishna1064@users.noreply.github.com>
2019-07-08 08:42:42 -0700

    remove comments

--- src/main/java/com/karvy/fileupload/exception/MyFileNotFoundException.java
+++ src/main/java/com/karvy/fileupload/exception/MyFileNotFoundException.java
@@ -5,9 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class MyFileNotFoundException extends RuntimeException {
-	
-	//File not found exception found here
-	
+		
     public MyFileNotFoundException(String message) {
         super(message);
     }
